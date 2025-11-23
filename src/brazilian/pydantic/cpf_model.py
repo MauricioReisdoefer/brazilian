1756e1dd2c2ddaeb0968669b38b238ceefb7cab8
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-11-22 21:29:19 -0300

    feat: pydantic model of CPF

---
+++ src/brazilian/pydantic/cpf_model.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, BeforeValidator
+from ..documents.cpf import CPF
+from typing import Annotated
+
+def validate_cpf(value):
+    cpf = CPF(value, strict=True)  
+    return cpf
+
+CPFType = Annotated[CPF, BeforeValidator(validate_cpf)]
+
+class CPFModel(BaseModel):
+    cpf: CPFType
