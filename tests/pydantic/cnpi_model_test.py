f763c01ff36b9824c798305af051a3eeb49d85c7
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-11-23 19:53:33 -0300

    test: testing cnpj pydantic model

---
+++ tests/pydantic/cnpi_model_test.py
@@ -0,0 +1,18 @@
+import pytest
+from pydantic import ValidationError
+from src.brazilian.pydantic.cnpj_model import CNPJModel 
+from src.brazilian.documents.cnpj import CNPJ
+
+def test_valid_cnpj():
+    model = CNPJModel(cnpj="45.723.174/0001-10")
+    assert isinstance(model.cnpj, CNPJ)
+    assert model.cnpj.formatted is not None
+
+def test_invalid_cnpj():
+    with pytest.raises(ValidationError):
+        CNPJModel(cnpj="asd98ds22")
+
+def test_json_encoder():
+    model = CNPJModel(cnpj="45.723.174/0001-10")
+    json_output = model.model_dump_json()
+    assert model.cnpj.formatted in json_output 
