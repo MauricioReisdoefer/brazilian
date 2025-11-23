0fa90b26d49baa236c81cbda05c23a80750ae8fe
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-11-22 21:31:21 -0300

    feat: pydantic model of CEP

---
+++ src/brazilian/pydantic/cep_model.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, BeforeValidator
+from ..documents.cep import CEP
+from typing import Annotated
+
+def validate_cep(value):
+    cep = CEP(value, strict=True)  
+    return cep
+
+CEPType = Annotated[CEP, BeforeValidator(validate_cep)]
+
+class CEPModel(BaseModel):
+    cep: CEPType
