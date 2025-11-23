47dada308f88f2e598754e3db176c8fe602ceeac
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-11-23 19:58:28 -0300

    test: adding crm pydantic test

---
+++ tests/pydantic/crm_model_test.py
@@ -0,0 +1,18 @@
+import pytest
+from pydantic import ValidationError
+from src.brazilian.pydantic.crm_model import CRMModel 
+from src.brazilian.documents.crm import CRM
+
+def test_valid_crm():
+    model = CRMModel(crm="123456/SC")
+    assert isinstance(model.crm, CRM)
+    assert model.crm.formatted is not None
+
+def test_invalid_crm():
+    with pytest.raises(ValidationError):
+        CRMModel(crm="abc1ass2345")
+
+def test_json_encoder():
+    model = CRMModel(crm="123456/SC")
+    json_output = model.model_dump_json()
+    assert model.crm.formatted in json_output 
